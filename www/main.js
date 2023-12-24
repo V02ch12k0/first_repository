1c9aac12e7d348f1d89a2eac2e6c38ae724fe8e3
V02ch12k0 <146691894+V02ch12k0@users.noreply.github.com>
2023-12-24 22:38:22 +0500

    лабораторная работа №4. JS - работа со стилями

---
+++ www/main.js
@@ -0,0 +1,32 @@
+function toggleVisibility() {
+    let element = document.getElementById('hiddenDiv');
+    if (element.style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
+
+closee.onmouseover = function() {
+    closee.style.background= "#f7dc63";
+};
+    
+closee.onmouseleave = function() {
+    closee.style.background= "grey";
+};
+
+save.onmouseover = function() {
+    save.style.background= "#f7dc63";
+};
+    
+save.onmouseleave = function() {
+    save.style.background= "grey";
+};
+
+show.onmouseover = function() {
+    show.style.background= "#f7dc63";
+};
+    
+show.onmouseleave = function() {
+    show.style.background= "grey";
+};
